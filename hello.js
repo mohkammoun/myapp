28edb2aee7c1c854c507192fd89f643a6cc5ab41
Mohamad Kammoun <46044320+mohkammoun@users.noreply.github.com>
2019-01-17 18:49:33 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,10 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Testing Message\n');
-}).listen(8080, '10.133.68.95');
-console.log('Server running at http://10.133.68.95:8080/');
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
